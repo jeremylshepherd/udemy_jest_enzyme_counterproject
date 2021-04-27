90b9575c2a355b01273139f6ca048806eaa886db
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-04-27 15:41:35 -0400

    First project in Udemy React Testing Jest & Enzyme start through 1.12

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+/* import { render, screen } from '@testing-library/react'; */
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 test('renders learn react link', () => {
+  /*  //Testing-Libray version
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  */
+
+  //Enzyme version
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
 });
