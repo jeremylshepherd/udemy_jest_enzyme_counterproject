f23c9d847a8c5892edde966c34163e95a081892b
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-04-28 11:07:38 -0400

    Completed Test suite for <App /> component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-/* import { render, screen } from '@testing-library/react'; */
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
-  /*  //Testing-Libray version
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  */
+const setup = () => shallow(<App />);
+const _$ = (wrapper, query: string) => wrapper.find(query);
 
-  //Enzyme version
-  const wrapper = shallow(<App />);
-  expect(wrapper.exists()).toBe(true);
+describe("The <App /> Component renders and behaves as expected", () => {
+  const wrapper = setup();
+  const button = _$(wrapper, `[data-test="button"]`);
+  const display = _$(wrapper, 'h1');
+
+  test('renders App component', () => {
+    const appComponent = _$(wrapper, `[data-test="App"]`);
+    expect(appComponent.exists()).toBe(true);
+  });
+
+  it('renders increment button', () => {  
+    expect(button.exists()).toBe(true);
+  });
+
+  it('renders Counter display', () => {  
+    expect(display.exists()).toBe(true);
+  });
+
+  it('Counter starts at 0', () => {
+    const counter = _$(wrapper, 'span');
+    expect(parseInt(counter.text())).toBe(0);
+  });
+
+  it('clicking button increments Counter Display', () => {
+    button.simulate('click');
+    const counter = _$(wrapper, 'span');
+    expect(parseInt(counter.text())).toBe(1);
+  });
+
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-test="App">
+      <h1>The counter is currently:&nbsp;<span>{count}</span></h1>
+      <button data-test="button" onClick={() => setCount(count + 1)}>Increment Count</button>
     </div>
   );
 }
