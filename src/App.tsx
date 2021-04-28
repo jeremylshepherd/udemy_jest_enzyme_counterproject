bd114e74cd3c7fbc3740fac0d63646e46086dfba
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-04-28 13:51:13 -0400

    :lipstick: Added bootstrap to quickly prettify UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
@@ -7,13 +8,15 @@ function App() {
   return (
     <div className="App" data-test="App">
       <h1>The counter is currently:&nbsp;<span data-test="counter-span">{count}</span></h1>
-      <div>
+      <div className="d-flex flex-column justify-content-center align-items-center">
         {
-          error ? <span data-test="error-span">You cannot decrement count below zero!</span>:
+          error ? <span className="alert alert-danger" data-test="error-span">You cannot decrement count below zero!</span>:
           null
         }
-        <button data-test="increment-button" onClick={() => {setCount(count + 1); setError(false);}}>Increment Count</button>
-        <button data-test="decrement-button" onClick={() => count > 0 ? setCount(count - 1) : setError(true)}>Decrement Button</button>
+        <div className="d-flex flex-row mt-5">
+          <button className="btn btn-primary mr-2" data-test="increment-button" onClick={() => {setCount(count + 1); setError(false);}}>Increment Count</button>
+          <button className="btn btn-danger" data-test="decrement-button" onClick={() => count > 0 ? setCount(count - 1) : setError(true)}>Decrement Button</button>
+        </div>
       </div>
     </div>
   );
