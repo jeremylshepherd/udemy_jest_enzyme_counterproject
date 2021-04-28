a1288757d42a05b2b697594eaf5f0339c040a026
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-04-28 12:29:49 -0400

    Challenge tests written and failing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,7 +10,8 @@ const $find = (wrapper : Enzyme.ShallowWrapper<any, Readonly<{}>, React.Componen
 
 describe("The <App /> Component renders and behaves as expected", () => {
   const wrapper = setup();
-  const button = $find(wrapper, `[data-test="button"]`);
+  const incrementButton = $find(wrapper, `[data-test="increment-button"]`);
+  const decrementButton = $find(wrapper, `[data-test="decrement-button"]`);
   const display = $find(wrapper, 'h1');
 
   test('renders App component', () => {
@@ -19,7 +20,7 @@ describe("The <App /> Component renders and behaves as expected", () => {
   });
 
   it('renders increment button', () => {  
-    expect(button.exists()).toBe(true);
+    expect(incrementButton.exists()).toBe(true);
   });
 
   it('renders Counter display', () => {  
@@ -32,9 +33,41 @@ describe("The <App /> Component renders and behaves as expected", () => {
   });
 
   it('clicking button increments Counter Display', () => {
-    button.simulate('click');
+    incrementButton.simulate('click');
     const counter = $find(wrapper, 'span');
     expect(parseInt(counter.text())).toBe(1);
+  });
+
+  //Counter Challenges Section
+  //Implement a Decrement Button
+
+  it('renders a decrement button', () => {
+    expect(decrementButton.exists()).toBe(true);
+  });
+
+  //Prevent Count from going negative and display error 
+  it('Count will not decrement below 0', () => {
+    const startingCount = +($find(wrapper, 'span').text());
+    console.log("starting count: ", startingCount);
+    decrementButton.simulate('click');
+    const counter = $find(wrapper, 'span');
+    expect(parseInt(counter.text())).toBe(0);
+  });
+
+  it('Displays error on if decrement button is clicked while count is 0', () => {
+    decrementButton.simulate('click');
+    const errorMessage = $find(wrapper, `[data-test="error-span"]`);
+    expect(errorMessage.exists()).toBe(false);
+  });
+
+  //Clear Error on Increment
+  it('Clears error on Increment', () => {
+    //Generate Error
+    decrementButton.simulate('click');
+    //Optionally verify error message present
+    incrementButton.simulate('click');
+    const postIncrementErrorMessage = $find(wrapper, `[data-test="error-span"]`);
+    expect(postIncrementErrorMessage.exists()).toBe(false);
   });
 
 });
